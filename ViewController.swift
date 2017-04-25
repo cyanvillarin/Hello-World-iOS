0b6f6465399af19087a8c8f8cca10d124654b91f
Cyan Vee <cyanvillarin@gmail.com>
2017-04-25 11:36:53 +0800

    Added Files
    This is for the Hello World folder only. Not including the .xcodeproj
    file and the Hello WorldTests and Hello WorldUITests folders.

---
+++ ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Cyan Villarin on 4/18/17.
+//  Copyright © 2017 Cyan Villarin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        box1.text = "Hello World!"
+    }
+    
+    @IBOutlet weak var box1: UILabel!
+    
+}
+
